ce9dca9a9851f9eed6a2ab0ea1f4c33958f707a5
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-10 15:31:01 -0500

    commit tree 2dba5ac2ce8527cd3f003ef12ef632ea02cd643c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,13 +2,38 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  def full_name
-    "#{self.first_name} #{self.last_name}" 
-  end
+  # def full_name
+  #   "#{self.first_name} #{self.last_name}" 
+  # end
 
-  def list_roles
-    self.characters.collect do |c|
-      "#{c.name} - #{c.show.name}"
-    end.join
-  end
+  # def list_roles
+  #   self.characters.collect do |c|
+  #     "#{c.name} - #{c.show.name}"
+  #   end.join
+  # end
+
+def full_name
+  "#{self.first_name} #{self.last_name}"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,11 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
   
+  # def say_that_thing_you_say
+  #   "#{self.name} always says: #{self.catchphrase}"
+  # end
+
+
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
